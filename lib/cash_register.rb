39ddeb2f2f018c5ea26e43365b4306ee14bf539f
Robert Tate <btate712@gmail.com>
2019-03-13 22:49:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
   def apply_discount
     if @discount != 0
       output_string =
-        "After the discount, the total comes to $#{@total *= (100-@discount).to_f/100.to_i}."
+        "After the discount, the total comes to $#{(@total *= (100-@discount).to_f/100).to_i}."
     else
       output_string = "There is no discount to apply."
     end
